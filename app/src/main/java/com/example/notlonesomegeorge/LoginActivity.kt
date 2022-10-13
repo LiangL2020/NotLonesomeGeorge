7f8c035c15de8b40adfbe06735bb04f3e76b8c3d
llu2 <llu2@wpi.edu>
2022-10-12 21:25:10 -0400

    login gui designed

---
+++ app/src/main/java/com/example/notlonesomegeorge/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.notlonesomegeorge
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class LoginActivity : AppCompatActivity() {
+
+    //DECLARE VARIABLES
+    var activityTag = "activityTag";
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i(activityTag, "In onCreate() LoginActivity...")
+    }
+}
